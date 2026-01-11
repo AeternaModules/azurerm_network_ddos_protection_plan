7c765169609ee833aa8f93b2788f56ec96327f4c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:51:32 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_ddos_protection_plans" {
+  description = "All network_ddos_protection_plan resources"
+  value       = azurerm_network_ddos_protection_plan.network_ddos_protection_plans
+}
+
